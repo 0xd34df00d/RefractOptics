9b37205288cbbccee3851084be4c79b6a20ca5d6
0xd34df00d <0xd34df00d@gmail.com>
2013-07-07 19:40:47 +0400

    Keep all coeffs; keep both formulas, removed SVM.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,42 +4,79 @@
 #include <thread>
 #include <mutex>
 #include <future>
+#include <limits>
 #include <dlib/svm.h>
 
 typedef dlib::matrix<double, 1, 1> SampleType_t;
 
-const size_t PowLength = 3;
-typedef dlib::matrix<double, PowLength, 1> Params_t;
+const size_t ParamsCount = 3;
+typedef dlib::matrix<double, ParamsCount, 1> Params_t;
 
-double residual (const std::pair<SampleType_t, double>& data, const Params_t& p)
+namespace Series
 {
-	double result = 0;
-	const auto x = data.first (0);
-	for (size_t i = 0; i < PowLength; ++i)
-		result += p (i) / std::pow (x, 2 * i);
-	return result - data.second;
+	double residual (const std::pair<SampleType_t, double>& data, const Params_t& p)
+	{
+		double result = 0;
+		const auto x = data.first (0);
+		for (size_t i = 0; i < ParamsCount; ++i)
+			result += p (i) / std::pow (x, 2 * i);
+		return result - data.second;
+	}
+
+	Params_t residualDer (const std::pair<SampleType_t, double>& data, const Params_t& p)
+	{
+		Params_t res;
+		const auto x = data.first (0);
+		for (size_t i = 0; i < ParamsCount; ++i)
+			res (i) = 1 / std::pow (x, 2 * i);
+		return res;
+	}
 }
 
-Params_t residualDer (const std::pair<SampleType_t, double>& data, const Params_t& p)
+namespace Resonance
 {
-	Params_t res;
-	const auto x = data.first (0);
-	for (size_t i = 0; i < PowLength; ++i)
-		res (i) = 1 / std::pow (x, 2 * i);
-	return res;
+	double residual (const std::pair<SampleType_t, double>& data, const Params_t& p)
+	{
+		const auto x = data.first (0);
+		const auto x2 = x * x;
+
+		const double cminus = p (2) - 1.0 / x2;
+		const double underRoot = p (0) + p (1) / cminus;
+		const double root = underRoot >= 0 ? std::sqrt(underRoot) : std::numeric_limits<double>::max () / 10;
+
+		return root - data.second;
+	}
+
+	Params_t residualDer (const std::pair<SampleType_t, double>& data, const Params_t& p)
+	{
+		const auto x = data.first (0);
+		const auto x2 = x * x;
+
+		const double cminus = p (2) - 1.0 / x2;
+		const double underRoot = p (0) + p (1) / cminus;
+		const double root = underRoot >= 0 ? std::sqrt(underRoot) : std::numeric_limits<double>::max () / 10;
+
+		Params_t res;
+		res (0) = 1. / (2. * root);
+		res (1) = 1. / (2. * root * cminus);
+		res (2) = -(p (1) / (2 * root * cminus * cminus));
+		return res;
+	}
 }
+
+using namespace Resonance;
 
 Params_t solve (const std::vector<std::pair<SampleType_t, double>>& pairs)
 {
 	Params_t p;
-	for (size_t i = 0; i < PowLength; ++i)
-		p (i) = 0;
+	for (size_t i = 0; i < ParamsCount; ++i)
+		p (i) = 1;
 	dlib::solve_least_squares_lm (dlib::objective_delta_stop_strategy(1e-5),
 			residual, residualDer, pairs, p);
 	return p;
 }
 
-typedef std::vector<dlib::running_stats<double>> StatsVec_t;
+typedef std::vector<std::vector<double>> StatsVec_t;
 
 StatsVec_t getStats (double lVar, double nVar,
 		const std::vector<std::pair<SampleType_t, double>>& pairs)
@@ -49,7 +86,7 @@ StatsVec_t getStats (double lVar, double nVar,
 	std::normal_distribution<double> nDistr (0, nVar);
 
 	StatsVec_t stats;
-	stats.resize (PowLength);
+	stats.resize (ParamsCount);
 	for (size_t i = 0; i < 10000; ++i)
 	{
 		std::vector<std::pair<SampleType_t, double>> localPairs = pairs;
@@ -60,10 +97,9 @@ StatsVec_t getStats (double lVar, double nVar,
 		}
 
 		const auto& p = solve (localPairs);
-		for (size_t j = 0; j < PowLength; ++j)
-			stats [j].add (p (j));
+		for (size_t j = 0; j < ParamsCount; ++j)
+			stats [j].push_back (p (j));
 	}
-
 	return stats;
 }
 
@@ -102,13 +138,21 @@ std::string format (double value)
 	return ostr.str ();
 }
 
-int main ()
+int main (int argc, char **argv)
 {
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << argv [0] << " datafile" << std::endl;
+		return 1;
+	}
+
+	const std::string infile (argv[1]);
+
 	std::vector<SampleType_t> samples;
 	std::vector<double> labels;
 	std::vector<std::pair<SampleType_t, double>> pairs;
 
-	std::ifstream istr("data/p1.txt");
+	std::ifstream istr (infile);
 	while (istr)
 	{
 		double lambda, n;
@@ -125,23 +169,6 @@ int main ()
 
 	std::cout << "read " << samples.size () << " samples: " << std::endl;
 
-	/*
-	{
-		typedef dlib::radial_basis_kernel<SampleType_t> Kernel_t;
-
-		dlib::svr_trainer<Kernel_t> trainer;
-		trainer.set_kernel (Kernel_t (2e-6));
-		trainer.set_c (1);
-		trainer.set_epsilon_insensitivity (1e-9);
-
-		auto df = trainer.train (samples, labels);
-		std::cout << "alpha: " << df.alpha << "; b: " << df.b << "; svs: " << df.basis_vectors.size() << std::endl;
-
-		dlib::randomize_samples (samples, labels);
-		std::cout << "MSE and R-squared " << dlib::cross_validate_regression_trainer (trainer, samples, labels, 2) << std::endl;
-	}
-	*/
-
 	{
 		const auto& p = solve (pairs);
 		std::cout << "inferred params: " << dlib::trans (p) << std::endl;
@@ -157,23 +184,39 @@ int main ()
 
 	std::cout << "calculating mean/dispersion..." << std::endl;
 
-	std::vector<double> lVars { 0, 0.01, 0.1, 1, 10 };
-	std::vector<double> nVars { 0, 1e-5, 2e-5, 5e-5, 1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 7e-3, 1e-2, 2e-2, 3e-2, 4e-2, 5e-2, 6e-2, 7e-2, 8e-2, 9e-2, 0.1 };
+	std::vector<double> lVars { 0, 0.1, 0.3, 1, 3 };
+	std::vector<double> nVars { 0, 1e-5, 2e-5, 3e-5, 4e-5, 5e-5, 6e-5, 7e-5, 8e-5, 9e-5,
+			1e-4, 2e-4, 3e-4, 4e-4, 5e-4, 6e-4, 7e-4, 8e-4, 9e-4, 1e-3 };
+	/*
+	std::vector<double> nVars { 0, 1e-5, 2e-5, 5e-5, 1e-4, 2e-5, 5e-4, 1e-3, 2e-3, 5e-3,
+			7e-3, 0.01, 0.01, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1 };
+			*/
 
 	std::mutex mapMutex;
 	std::map<double, std::map<double, std::vector<dlib::running_stats<double>>>> results;
 	for (auto lVar : lVars)
 	{
-		std::map<double, std::future<StatsVec_t>> nVar2future;
-		for (auto nVar : nVars)
-			nVar2future [nVar] = std::async (std::launch::async, getStats, lVar, nVar, pairs);
+		for (auto i = nVars.begin (); i != nVars.end (); )
+		{
+			std::map<double, std::future<StatsVec_t>> nVar2future;
+			for (size_t t = 0; i != nVars.end () && t < std::thread::hardware_concurrency (); ++i, ++t)
+				nVar2future [*i] = std::async (std::launch::async, getStats, lVar, *i, pairs);
 
-		for (auto nVar : nVars)
-			results [lVar] [nVar] = nVar2future [nVar].get ();
+			for (auto& pair : nVar2future)
+			{
+				const auto& coeffs = pair.second.get ();
+				std::vector<dlib::running_stats<double>> stats;
+				stats.resize (coeffs.size ());
+				for (size_t i = 0; i < coeffs.size (); ++i)
+					for (auto coeff : coeffs [i])
+						stats [i].add (coeff);
+				results [lVar] [pair.first] = stats;
+			}
+		}
 	}
 
 	/*
-	for (size_t i = 0; i < PowLength; ++i)
+	for (size_t i = 0; i < ParamsCount; ++i)
 	{
 		std::cout << "\\begin{tabular}{| l ";
 		for (size_t i = 0; i < nVars.size (); ++i)
@@ -200,10 +243,10 @@ int main ()
 	}
 	*/
 
-	for (size_t i = 0; i < PowLength; ++i)
+	for (size_t i = 0; i < ParamsCount; ++i)
 	{
 		std::stringstream fname;
-		fname << "coeff" << i << ".dat";
+		fname << infile << "_coeff" << i << ".dat";
 
 		std::ofstream ostr (fname.str ());
 		for (auto lVar : lVars)
