70a8126be607456d273c9f15be1cfcc61cd160dd
0xd34df00d <0xd34df00d@gmail.com>
2015-05-23 16:07:13 +0300

    500 items for Solve.

--- cpp/solve.h
+++ cpp/solve.h
@@ -72,7 +72,7 @@ Params_t<ParamsCount> solve (const TrainingSet_t& pairs,
 
 	const auto diff = 1e-12;
 
-	for (int i = 0; i < 200; ++i)
+	for (int i = 0; i < 500; ++i)
 	{
 		const auto prevP = p;
 
