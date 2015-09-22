c9e285d54eefbd269845be179e008e734c0b1d67
0xd34df00d <0xd34df00d@gmail.com>
2015-09-23 02:15:30 +0300

    Proper symbol name in convergence script.

--- cpp/data/laser/convergence/convergence_script.sh
+++ cpp/data/laser/convergence/convergence_script.sh
@@ -8,6 +8,6 @@ set output '$1.pdf'
 set xtics offset 1,-0.5 font ",14"
 set ytics offset 0,-0.0 font ",14"
 set xlabel "k"
-set ylabel "{/Symbol w}_i / {/Symbol w}_{0i}"
+set ylabel "{/Symbol w}_i / {/Symbol w}@_i^0"
 plot "$1" using (\$1):(\$4/\$4) with lines title "Классическое значение", "$1" using (\$1):(\$5/\$2) with lines title "g_0", "$1" using (\$1):(\$6/\$3) with lines title "{/Symbol a}_0", "$1" using (\$1):(\$7/\$4) with lines title "{/Symbol g}"
 TOEND
