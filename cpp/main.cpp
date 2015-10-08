6f9a87fb1620aa90935b60cabe054a61450ca0c7
0xd34df00d <0xd34df00d@gmail.com>
2015-10-09 00:35:36 +0100

    [cpp] Repetitions count option.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -204,6 +204,7 @@ boost::program_options::variables_map parseOptions (int argc, char **argv)
 		("conv-step", po::value<double> (), "convergence step")
 		("values-start", po::value<double> (), "values start")
 		("values-end", po::value<double> (), "values end")
+		("repetitions", po::value<int> (), "repetitions count")
 		("multiplier", po::value<int> (), "set multiplier");
 
 	po::positional_options_description p;
