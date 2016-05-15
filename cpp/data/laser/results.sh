b09c0cee4ea2fcc5af8c90bc560d8378a93e32ba
0xd34df00d <0xd34df00d@gmail.com>
2016-05-14 23:34:13 -0400

    [cpp] Updated results script to have new values.

--- cpp/data/laser/results.sh
+++ cpp/data/laser/results.sh
@@ -12,5 +12,5 @@ set ylabel "y"
 set key bottom right
 set datafile separator ','
 f(x, g, a, y) = y * (1 - x) / (1 + x) * (g / (a - log (x) / 300) - 1)
-plot 'ldata.txt' using (\$1):(\$2):(\$1 > 0.6 ? 0.01 : 0.02):(\$2 * 0.02) with xyerrorbars title 'Экспериментальные данные', f(x, 0.00291712, 0.000221917, 101.543) title '{/Symbol w}^0', f(x, 0.00292006, 0.000215645, 100.324) title '{/Symbol w}'
+plot 'ldata.txt' using (\$1):(\$2):(\$1 > 0.6 ? 0.01 : 0.02):(\$2 * 0.02) with xyerrorbars title 'Экспериментальные данные', f(x, 0.00291712, 0.000221917, 101.543) title '{/Symbol w}^0', f(x, 0.00292629, 0.000207285, 98.5809) title '{/Symbol w}'
 TOEND
