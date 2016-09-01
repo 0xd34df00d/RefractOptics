67f39f7f589a54f67c45cd5803b567eb5c0e140f
0xd34df00d <0xd34df00d@gmail.com>
2016-08-31 20:34:14 -0400

    Added a script to generate the IPI publication archive.

---
+++ doc/make_ipi.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+TARGET=../ipi/src
+
+mkdir -p $TARGET
+cp bibliography.bib $TARGET
+cp slashbox.sty $TARGET
+cp Rudoy2014LevMar.tex $TARGET/Rudoy2016LevMar.tex				# meh
+cp Rudoy2014LevMar.pdf $TARGET/../Rudoy2016LevMar.pdf
+
+mkdir -p $TARGET/figs/levmar/convergence
+mkdir -p $TARGET/figs/levmar/comparison
+cp figs/levmar/results.pdf $TARGET/figs/levmar
+cp figs/levmar/convergence/*.pdf $TARGET/figs/levmar/convergence
+cp figs/levmar/comparison/*.pdf $TARGET/figs/levmar/comparison
+
+cd ../ipi
+zip -r Rudoy2016LevMar src/
