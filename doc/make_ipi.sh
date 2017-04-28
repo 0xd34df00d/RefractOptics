dca8be3470bf49cb63a9a3683427f142963a4a74
0xd34df00d <0xd34df00d@gmail.com>
2017-04-28 02:35:04 -0400

    Fixed IPI deployment script.

--- doc/make_ipi.sh
+++ doc/make_ipi.sh
@@ -1,18 +1,20 @@
 #! /bin/sh
 
-TARGET=../ipi/src
+TARGET=../ipi/Rudoy2016LevMar
 
 mkdir -p $TARGET
 cp bibliography.bib $TARGET
 cp slashbox.sty $TARGET
+cp authors.txt $TARGET/authors.txt
 cp Rudoy2014LevMar.tex $TARGET/Rudoy2016LevMar.tex				# meh
 cp Rudoy2014LevMar.pdf $TARGET/../Rudoy2016LevMar.pdf
 
 mkdir -p $TARGET/figs/levmar/convergence
 mkdir -p $TARGET/figs/levmar/comparison
 cp figs/levmar/results.pdf $TARGET/figs/levmar
+cp figs/levmar/results_bw.pdf $TARGET/figs/levmar
 cp figs/levmar/convergence/*.pdf $TARGET/figs/levmar/convergence
 cp figs/levmar/comparison/*.pdf $TARGET/figs/levmar/comparison
 
 cd ../ipi
-zip -r Rudoy2016LevMar src/
+zip -r Rudoy2016LevMar Rudoy2016LevMar/
